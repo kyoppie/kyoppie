70ee348e3b382eb1cd4a65fce546fb4271fe0462
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-04 20:32:46 +0900

    Web向けコンフィグを吐き出すように
    これ以前にmigrateした人はもう一回migrateしてね〜

--- api/src/migrate/20160820_200318_add_web_app.migrate.js
+++ api/src/migrate/20160820_200318_add_web_app.migrate.js
@@ -1,4 +1,5 @@
 var newApplicationKey = require("../utils/newApplicationKey");
+var fs = require("fs");
 exports.up = function(models){
     // write your migrate
     var app = new models.apps();
@@ -6,6 +7,16 @@ exports.up = function(models){
     app.appKey = newApplicationKey();
     app.appSecret = newApplicationKey();
     app.isWeb = true;
+    var config;
+    var configFilePath = __dirname+"/../../web_config.json";
+    try{
+        config = JSON.parse(fs.readFileSync(configFilePath));
+    } catch (e){
+        config = {};
+    }
+    config.appKey = app.appKey;
+    config.appSecret = app.appSecret;
+    fs.writeFileSync(configFilePath,JSON.stringify(config,null,4));
     return models.users.find({
         adminLevel:-1
     }).sort({createdAt:1}).limit(1).then(function(user){
