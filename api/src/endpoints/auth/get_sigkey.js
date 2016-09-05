46697ac0115dda4dd10e4694449c66d385686054
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-05 18:16:50 +0900

    署名キー取得APIを実装

---
+++ api/src/endpoints/auth/get_sigkey.js
@@ -0,0 +1,18 @@
+var models = require("../../models")
+var newSignatureKey = require("../../utils/newSignatureKey")
+module.exports = function(appKey){
+    var sig;
+    return new Promise(function(resolve,reject){
+        if(!appKey) return reject("appKey-is-require");
+        models.apps.findOne({appKey}).then(resolve,reject);
+    }).then(function(app){
+        if(!app) return Promise.reject("app-is-not-found");
+        sig = new models.signatures();
+        sig.appKey = appKey;
+        sig.sigKey = newSignatureKey();
+        sig.sigHash = newSignatureKey();
+        return sig.save();
+    }).then(function(){
+        return Promise.resolve(sig)
+    });
+}
