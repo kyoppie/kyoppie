dc87c4e9d0277153896e671178a33ce34fba1030
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-06 12:03:14 +0900

    fix indent

--- api/src/endpoints/users/show.js
+++ api/src/endpoints/users/show.js
@@ -1,19 +1,19 @@
 var models = require("../../models")
 module.exports = function(screenName,id){
     return new Promise(function(resolve,reject){
-      if(!screenName && !id){
-        return reject("screenName-or-id-require")
-      }
-      function retUser(err,res){
-        if(!res){
-          return reject("not-found-user")
+        if(!screenName && !id){
+            return reject("screenName-or-id-require")
         }
-        return resolve(res)
-      }
-      if(screenName) {
-        models.users.findOne({screenNameLower:screenName.toLowerCase()},retUser)
-      } else {
-        models.users.findOne({_id:models.mongoose.Types.ObjectId(id)},retUser)
-      }
+        function retUser(err,res){
+            if(!res){
+                return reject("not-found-user")
+            }
+            return resolve(res)
+        }
+        if(screenName) {
+            models.users.findOne({screenNameLower:screenName.toLowerCase()},retUser)
+        } else {
+            models.users.findOne({_id:models.mongoose.Types.ObjectId(id)},retUser)
+        }
     })
 }
