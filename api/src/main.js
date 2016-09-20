f40b3474dac40e16142773bb5733713e43a0fa7c
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-20 16:21:04 +0900

    support accessToken auth and CORS

--- api/src/main.js
+++ api/src/main.js
@@ -4,14 +4,35 @@ var models = require("./models")
 var bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
+app.use(function(req,res,next){
+    res.append("Access-Control-Allow-Origin","*")
+    res.append("Access-Control-Allow-Method","GET, POST, OPTIONS");
+    res.append("Access-Control-Allow-Headers","X-Kyoppie-Access-Token");
+    res.append("Access-Control-Max-Age","86400")
+    next();
+})
+
 var routes = require("./routes")
 routes.forEach(function(route){
     var login = true;
-if(route.login !== undefined) login = route.login;
+    if(route.login !== undefined) login = route.login;
     var method = route.method;
     var path = route.name;
     app[method](path,function(req,res,next){
-        next();
+        if(req.headers["x-kyoppie-access-token"]){
+            models.access_tokens.findOne({
+                secret:req.headers["x-kyoppie-access-token"]
+            }).populate("app user").then(function(token){
+                if(token){
+                    req.token=token;
+                }
+                next();
+            },function(){
+                res.status(500).send({response:false,error:"server-side-auth-error"})
+            })
+        } else {
+            next();
+        }
     },require("./handlers/web"+path));
 })
 app.all("*",function(req,res){
