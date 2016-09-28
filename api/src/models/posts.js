cfb19d53018f53f086aec58446f695c1e8561093
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-09-28 10:46:54 +0900

    support text_html

--- api/src/models/posts.js
+++ api/src/models/posts.js
@@ -17,6 +17,13 @@ module.exports = function(mongoose) {
             delete obj.app.appKey;
             delete obj.app.appSecret;
         }
+        obj.text_html = obj.text;
+        obj.text_html = obj.text_html.split('&').join("&amp;")
+        obj.text_html = obj.text_html.split("<").join("&lt;")
+        obj.text_html = obj.text_html.split(">").join("&gt;")
+        // obj.text_html = obj.text_html.split('"').join("&quot;")
+        obj.text_html = obj.text_html.replace(/(^| |　)@([A-Za-z0-9_]+)/g,'$1<a href="/u/$2">@$2</a>');
+        obj.text_html = obj.text_html.replace(/(https?:\/\/[a-zA-Z0-9-\.]+(:[0-9]+)?(\/?[a-zA-Z0-9-\._~\!#$&'\(\)\*\+,\/:;=\?@\[\]]*))/g,'<a href="$1" rel="nofollow" target="_blank">$1</a>');
         return obj;
     }
     return mongoose.model("posts",schema)
