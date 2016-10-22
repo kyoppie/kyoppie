64f023b153e3b7db80c8bc863878893acdcf0fa6
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-10-22 13:23:34 +0900

    fix migrate

--- api/src/migrate/20161020_224328_user.isSuspended.migrate.js
+++ api/src/migrate/20161020_224328_user.isSuspended.migrate.js
@@ -1,6 +1,6 @@
 exports.up = function(models){
     // write your migrate
-    return models.users.find().then(function(users){
+    return models.users.find({isSuspended:{$exists:false}}).then(function(users){
         var promises = [];
         users.forEach(function(user){
             user.isSuspended = !!user.isSuspended;
