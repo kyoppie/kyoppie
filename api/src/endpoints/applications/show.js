fa8e4f135b3214d3f4edadb4ca712751344a3276
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-11-04 18:22:43 +0900

    applications/showが使えなかった不具合を修正

--- api/src/endpoints/applications/show.js
+++ api/src/endpoints/applications/show.js
@@ -1,6 +1,6 @@
 var models = require("../../models")
 module.exports = function* (token,id){
-    var app = models.apps.findById({
+    var app = yield models.apps.findById({
         _id:id
     })
     if(app.user != token.user.id){
