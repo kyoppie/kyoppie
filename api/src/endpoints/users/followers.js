8fa1f760b2cef6a67e5657e48862e2288d749a38
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-11-07 09:34:47 +0900

    フォロワー一覧が取得できない問題を修正

--- api/src/endpoints/users/followers.js
+++ api/src/endpoints/users/followers.js
@@ -3,14 +3,16 @@ module.exports = function* (screenName,id){
     if(!screenName && !id) return Promise.reject("screenName-or-id-require")
     var user;
     if(screenName) {
-        user = yield models.users.findOne({screenNameLower:screenName.toLowerCase()}).populate("avatar")
+        user = yield models.users.findOne({screenNameLower:screenName.toLowerCase()})
     } else {
-        user = yield models.users.findOne({_id:models.mongoose.Types.ObjectId(id)}).populate("avatar")
+        user = yield models.users.findOne({_id:models.mongoose.Types.ObjectId(id)})
     }
     if(!user) return Promise.reject("user-not-found")
     if(user.isSuspended) return Promise.reject("this-user-is-suspended")
     var followers = yield models.follows.find({
         toUser:user.id
     }).populate("fromUser")
-    return followers.filter((follower)=>{return !follower.isSuspended})
+    followers = followers.map(follow => follow.fromUser).filter(user => !user.isSuspended)
+    console.log(followers)
+    return followers;
 }
