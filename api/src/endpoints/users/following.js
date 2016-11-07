3e2780d2d050df8f5266d94edee39bb452ec2e44
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-11-07 09:34:56 +0900

    following一覧を実装

--- api/src/endpoints/users/following.js
+++ api/src/endpoints/users/following.js
@@ -1,23 +1,17 @@
 var models = require("../../models")
-module.exports = function(screenName,id){
+module.exports = function* (screenName,id){
     if(!screenName && !id) return Promise.reject("screenName-or-id-require")
-    var promise;
+    var user;
     if(screenName) {
-        promise = models.users.findOne({screenNameLower:screenName.toLowerCase()}).populate("avatar")
+        user = yield models.users.findOne({screenNameLower:screenName.toLowerCase()}).populate("avatar")
     } else {
-        promise = models.users.findOne({_id:models.mongoose.Types.ObjectId(id)}).populate("avatar")
+        user = yield models.users.findOne({_id:models.mongoose.Types.ObjectId(id)}).populate("avatar")
     }
-    return promise.then(function(user){
-        if(!user) return Promise.reject("user-not-found")
-        if(user.isSuspended) return Promise.reject("this-user-is-suspended")
-        return models.follows.find({
-            fromUser:user.id
-        }).populate("toUser")
-    }).then(function(follows){
-        var following_users = [];
-        follows.forEach(function(follow){
-            if(!follow.toUser.isSuspended) following_users.push(follow.toUser);
-        })
-        return Promise.resolve(following_users)
-    })
+    if(!user) return Promise.reject("user-not-found")
+    if(user.isSuspended) return Promise.reject("this-user-is-suspended")
+    var follows = yield models.follows.find({
+        fromUser:user.id
+    }).populate("toUser")
+    follows = follows.map(follow => follow.toUser).filter(user => !user.isSuspended)
+    return follows;
 }
