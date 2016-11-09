0cf4f3b1675c26541a9af7d4d023c0916caa0d5d
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-11-09 17:45:59 +0900

    fix applications/create bug

--- api/src/endpoints/applications/create.js
+++ api/src/endpoints/applications/create.js
@@ -1,7 +1,7 @@
 var models = require("../../models")
 module.exports = function* (token,name,isWeb = false){
     if(!name) return Promise.reject("name-is-require");
-    const app = yield models.apps.findOne({name});
+    var app = yield models.apps.findOne({name});
     if(app) return Promise.reject("this-name-is-already-used")
     app = new models.apps();
     app.user = token.user.id;
