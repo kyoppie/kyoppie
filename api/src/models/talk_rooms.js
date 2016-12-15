dc3cea36ae71511d0580eba90204c67cea6e78f5
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-12-15 10:59:35 +0900

    talk_roomsモデルを修正

--- api/src/models/talk_rooms.js
+++ api/src/models/talk_rooms.js
@@ -6,5 +6,21 @@ module.exports = function(mongoose) {
     },{
         timestamps:true
     })
+    schema.methods.toResponseObject = function* (token) {
+        var obj = this.toObject()
+        obj.id = this._id
+        obj._id = undefined
+        obj.__v = undefined
+        if (this.users && this.users.length) {
+            for (let i=0;i<this.users.length;i++) {
+                if (!this.users[i].toResponseObject) {
+                    // obj.users = []
+                    break
+                }
+                obj.users[i] = yield this.users[i].toResponseObject(token)
+            }
+        }
+        return obj
+    }
     return mongoose.model("talk_rooms",schema)
 }
