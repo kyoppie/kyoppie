217b055ba10e7fc16f1b7dfe85fa66e6b9b37d85
Petitsurume <gamemannew2013@yahoo.co.jp>
2016-12-15 10:08:31 +0900

    talk_messagesモデルを追加

---
+++ api/src/models/talk_messages.js
@@ -0,0 +1,12 @@
+module.exports = function(mongoose){
+    var schema = new mongoose.Schema({
+        room:{type:mongoose.Schema.Types.ObjectId,ref:"talk_rooms"},
+        user:{type:mongoose.Schema.Types.ObjectId,ref:"users"},
+        text:String,
+        files:[{type:mongoose.Schema.Types.ObjectId,ref:"files"}],
+        app:{type:mongoose.Schema.Types.ObjectId,ref:"apps"},
+    },{
+        timestamps:true
+    });
+    return mongoose.model("talk_messages",schema);
+}
