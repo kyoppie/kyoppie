082c9f767cf6272e5f9f320b60be8948ee61f902
petitsurume <gamemannew2013@yahoo.co.jp>
2017-01-26 10:28:33 +0900

    fix error message

--- api/src/endpoints/account/update/avatar.js
+++ api/src/endpoints/account/update/avatar.js
@@ -2,7 +2,7 @@ var models = require("../../../models")
 module.exports = async function (token,file) {
     if (!file) return Promise.reject("file-is-required")
     file = await models.files.findById(file)
-    if (!file) return Promise.reject("invalid-file")
+    if (!file) return Promise.reject("file-not-found")
     if (file.type != "image") return Promise.reject("invalid-file")
     var user = token.user
     user.avatar = file.id
