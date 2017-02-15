ab99993c37f0d8d0686d45192d1f09f0499d25d7
otofune <3gakugd@gmail.com>
2017-02-15 00:44:52 +0000

    mongodbの生存確認をするように

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,6 +1,11 @@
 var mongoose = require("mongoose")
 var fs = require("fs")
 mongoose.connect("mongodb://"+(process.env.MONGO_HOST || "localhost")+"/"+(process.env.MONGO_DBNAME || "kyoppie_api"))
+.catch(e => {
+    console.error("Error: " + e.message)
+    console.error("process will exit because of mongodb is dead.")
+    process.exit()
+})
 var files = fs.readdirSync(__dirname)
 var db={}
 files.forEach(function(file) {
