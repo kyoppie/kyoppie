e3d377cae5e8b7b0b17f0bfa00652fb72b4b9567
Petitsurume <gamemannew2013@yahoo.co.jp>
2017-04-06 03:57:19 +0900

    [WIP] APIのモデル修正

--- api/src/models/talk_messages.js
+++ api/src/models/talk_messages.js
@@ -8,5 +8,31 @@ module.exports = function(mongoose) {
     },{
         timestamps:true
     })
+    schema.methods.toResponseObject = async function (token) {
+        var obj = this.toObject()
+        obj.id = this._id
+        delete obj._id
+        delete obj.__v
+        if (this.user.toResponseObject) obj.user=await this.user.toResponseObject(token)
+        else {
+            this.user = await mongoose.model("users").findById(this.user)
+            if (this.user) obj.user = await this.user.toResponseObject(token)
+        }
+        if (this.app && this.app.toResponseObject) {
+            obj.app=await this.app.toResponseObject(token)
+            delete obj.app.appKey
+            delete obj.app.appSecret
+        }
+        if (this.files && this.files.length) {
+            for (let i=0;i<this.files.length;i++) {
+                if (!this.files[i].toResponseObject) {
+                    obj.files = []
+                    break
+                }
+                obj.files[i] = await this.files[i].toResponseObject(token)
+            }
+        }
+        return obj
+    }
     return mongoose.model("talk_messages",schema)
 }
