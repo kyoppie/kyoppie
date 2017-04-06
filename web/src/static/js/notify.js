e3f654c537dfd534466ba13d8c276e8940e33843
petitsurume <gamemannew2013@yahoo.co.jp>
2017-04-06 19:16:33 +0900

    showNotifyに直接hideNotifyまでのミリ秒を指定できるように

--- web/src/static/js/notify.js
+++ web/src/static/js/notify.js
@@ -1,7 +1,8 @@
 $.extend({
-    showNotify:function(str){
+    showNotify:function(str,millisec){
         $("#notify").text(str);
         $("#notify").addClass("active");
+        if(millisec) setTimeout($.hidenotify, millisec)
     },
     hideNotify:function(){
         $("#notify").removeClass("active");
