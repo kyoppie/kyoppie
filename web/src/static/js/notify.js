6b9b2f4172fc720722f07b0f9857f2fd290b2ca1
petitsurume <gamemannew2013@yahoo.co.jp>
2017-04-06 19:29:15 +0900

    バグ直した

--- web/src/static/js/notify.js
+++ web/src/static/js/notify.js
@@ -2,7 +2,7 @@ $.extend({
     showNotify:function(str,millisec){
         $("#notify").text(str);
         $("#notify").addClass("active");
-        if(millisec) setTimeout($.hidenotify, millisec)
+        if(millisec) setTimeout($.hideNotify, millisec)
     },
     hideNotify:function(){
         $("#notify").removeClass("active");
