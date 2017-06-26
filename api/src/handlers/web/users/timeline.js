f5d8c79ccd8bae91c71551de26ecf61408249257
petitsurume <git@surume.tk>
2017-06-27 05:41:24 +0900

    users/timelineにlimit追加

--- api/src/handlers/web/users/timeline.js
+++ api/src/handlers/web/users/timeline.js
@@ -4,6 +4,7 @@ var wrap = require("../wrap")
 module.exports = async function () {
     await wrap(main(
         this.request.query.screenName,
-        this.request.query.id
+        this.request.query.id,
+        this.request.query.limit
     ),this)
 }
