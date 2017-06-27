0a5d2cbefe074b03207930cc96929d33b111268b
petitsurume <git@surume.tk>
2017-06-27 09:31:47 +0900

    fix bug

--- api/src/migrate/20170619_073635_user_password_version.migrate.js
+++ api/src/migrate/20170619_073635_user_password_version.migrate.js
@@ -1,6 +1,6 @@
 exports.up = function(models) {
     // write your migrate
-    return models.users.update({}, {$set: {
+    return models.users.update({passwordVersion: {$nin: [2]}}, {$set: {
         passwordVersion: 1
     }}, {multi: true})
 }
